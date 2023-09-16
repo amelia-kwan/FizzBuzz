e6896fd7505c566a0c1be054675867061f58143d
amelia-kwan <91842034+amelia-kwan@users.noreply.github.com>
2023-09-16 15:49:59 -0400

    changed for loop into while loop in FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,11 +1,11 @@
 /**
- * Solve the FizzBuzz challenge.
- */
+
+ Solve the FizzBuzz challenge.*/
 class FizzBuzz {
 
     public static void main(String[] args) {
-
-        for (int i = 1; i < 100; i++) {
+        int i = 1;
+        while (i < 101) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -29,6 +29,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
     }
 }
